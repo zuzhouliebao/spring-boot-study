71ea9869d35f203211f55d7780e104f8dfc73e9d
Administrator <2478794031@qq.com>
2019-03-18 10:08:48 +0800

    my commit

---
+++ quickstart/src/main/java/com/springboot/quickstart/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.springboot.quickstart.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * springboot的第一个restful请求
+ * @author
+ * 2019.3.18
+ * */
+@RestController
+public class HelloController {
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String getHello(){
+        return "Hello,Spring Boot..";
+    }
+
+}
